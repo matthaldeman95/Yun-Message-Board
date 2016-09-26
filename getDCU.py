f920ffef945d8b1188db19f88dee8b197182fee9
Matthew Haldeman <matthaldeman95@gmail.com>
2016-09-26 16:24:40 -0400

    Added opponent to DCU. Added timeout to prevent freezing up when website is down

--- getDCU.py
+++ getDCU.py
@@ -1,86 +1,76 @@
 #!/usr/bin/python
 from bs import BeautifulSoup, newurllib2
-import datetime
+from timeout import timeout, TimeoutError
+from datetime import datetime as dt
+from calendar import getMonth
 
 Soup = BeautifulSoup.BeautifulSoup
-response = newurllib2.urlopen('http://www.dcunited.com/standings').read()
-soup = Soup(response)
-classes = Soup(str(soup.findAll('tr')))
 
-for c in classes:
-    c = Soup(str(c))
-    data = c.find("td", attrs={"data-title": "Club"})
-    if data:
-        data = data.contents
-        foo, data, bar = str(data).split("'")
-        if data=="D.C. United":
-            break
-        else: continue
 
-rank = str(c.find("td", attrs={"data-title": "Rank"}).contents)
-foo, rank, bar = rank.split("'")
-w = str(c.find("td", attrs={"data-title": "Wins"}).contents)
-foo, w, bar = w.split("'")
-l = str(c.find("td", attrs={"data-title": "Losses"}).contents)
-foo, l, bar = l.split("'")
-t = str(c.find("td", attrs={"data-title": "Ties"}).contents)
-foo, t, bar = t.split("'")
+def getRankRecord():
+    response = newurllib2.urlopen('http://www.dcunited.com/standings').read()
+    soup = Soup(response)
 
-response = newurllib2.urlopen('http://www.dcunited.com/schedule').read()
-soup = Soup(response)
+    classes = Soup(str(soup.findAll('tr')))
+    for c in classes:
+        c = Soup(str(c))
+        data = c.find("td", attrs={"data-title": "Club"})
+        if data:
+            data = data.contents
+            foo, data, bar = str(data).split("'")
+            if data == "D.C. United":
+                break
+            else:
+                continue
 
-upc = Soup(str(soup.find("li", attrs={"class": "last"})))
-date = str(upc.find("div", attrs={"class": "match_details upcoming"}).contents)
-foo, date, bar = date.split("'")
-day, date = date.split(", ")
-m, d = date.split(" ")
-d, bar = d.split("t")
-time = Soup(str(upc.find("div", attrs={"class": "timing"})))
-time = str(time.find("div", attrs={"class": "match_info"}).contents)
-foo, time, bar = time.split("'")
-time, bar = time.split("PM")
+    r = str(c.find("td", attrs={"data-title": "Rank"}).contents)
+    r = r.split("'")[1]
+    w = str(c.find("td", attrs={"data-title": "Wins"}).contents)
+    w = w.split("'")[1]
+    l = str(c.find("td", attrs={"data-title": "Losses"}).contents)
+    l = l.split("'")[1]
+    t = str(c.find("td", attrs={"data-title": "Ties"}).contents)
+    t = t.split("'")[1]
 
-if(m == "January"):
-    m = "Jan"
-    mon = 1
-elif(m == "February"):
-    m = "Feb"
-    mon = 2
-elif(m == "March"):
-    m = "Mar"
-    mon = 3
-elif(m == "April"):
-    m = "Apr"
-    mon = 4
-elif(m == "May"):
-    m = "May"
-    mon = 5
-elif(m == "June"):
-    m = "Jun"
-    mon = 6
-elif(m == "July"):
-    m = "Jul"
-    mon = 7
-elif(m == "August"):
-    m = "Aug"
-    mon = 8
-elif(m == "September"):
-    m = "Sep"
-    mon = 9
-elif(m == "October"):
-    m = "Oct"
-    mon = 10
-elif(m == "November"):
-    m = "Nov"
-    mon = 11
-elif(m == "December"):
-    m = "Dec"
-    mon = 12
+    return r, w, l, t
 
-today = datetime.date.today()
 
-print "DCU: (%s, %s-%s-%s) Next Match:" % (rank, w, l, t),
-if int(d) == today.day and mon == today.month:
-    print "Today, %s PM" % time
-else:
-    print "%s %s, %s PM" % (m, d, time)
+def getNextMatchInfo():
+
+    response = newurllib2.urlopen('http://www.dcunited.com/schedule').read()
+    soup = Soup(response)
+
+    upc = Soup(str(soup.find("li", attrs={"class": "last"})))
+    date = str(upc.find("div", attrs={"class": "match_details upcoming"}).contents)
+    date = date.split("'")[1]
+    date = date.split(", ")[1]
+    m, d = date.split(" ")
+    d = d.split("t")[0]
+    t = Soup(str(upc.find("div", attrs={"class": "timing"})))
+    t = str(t.find("div", attrs={"class": "match_info"}).contents)
+    t = t.split("'")[1]
+    t = t.split("PM")[0]
+
+    opp = str(upc.find("div", attrs={"class": "club_container clubtwo"}).contents)
+    opp = opp.split('title="')[1]
+    opp = opp.split('"')[0]
+
+    m_name, m_number = getMonth(m)
+
+    return d, m_name, m_number, t, opp
+
+
+if __name__ == "__main__":
+    with timeout(seconds=5):
+        try:
+            rank, wins, losses, ties = getRankRecord()
+            day, month_name, month_number, time, opponent = getNextMatchInfo()
+            today = dt.today()
+            print "DCU: (%s, %s-%s-%s):" % (rank, wins, losses, ties),
+            if int(day) == today.day and month_number == today.month:
+                print "Today, %s" % time,
+            else:
+                print "%s %s, %s" % (month_name, day, time),
+            print "vs", opponent
+        except TimeoutError:
+            print ""
