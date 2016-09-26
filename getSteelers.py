1c8975c42a9645aab28904e7d7d0dd88be0bf6fb
Matthew Haldeman <matthaldeman95@gmail.com>
2016-09-26 17:06:02 -0400

    Added timeout and opponent field

--- getSteelers.py
+++ getSteelers.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python
 from bs import BeautifulSoup, newurllib2
-import datetime
+from timeout import timeout, TimeoutError
+from datetime import datetime as dt
+
 Soup = BeautifulSoup.BeautifulSoup
-response = newurllib2.urlopen('http://www.nfl.com/teams/pittsburghsteelers/schedule?team=PIT').read()
-soup = Soup(response)
 
-ranking = str(soup.find("p", attrs={"class": "team-overall-ranking"}).contents)
+def getRankRecord():
+    response = newurllib2.urlopen('http://www.nfl.com/teams/pittsburghsteelers/schedule?team=PIT').read()
+    soup = Soup(response)
+
+    ranking = str(soup.find("p", attrs={"class": "team-overall-ranking"}).contents)
+
+    rk = (ranking.split("'")[1].strip()).split(" ")[0][0]
+    rec = ranking.split("'")[2].strip()
+    rec = (rec.split("(")[1]).split(")")[0].strip()
+
+    return rk, rec
+
+def getNextGame():
+    response = newurllib2.urlopen('http://www.sbnation.com/nfl/teams/pittsburgh-steelers').read()
+    soup = Soup(response)
+    soup = Soup(str(soup.find("div", attrs={"class": "sbn-pte-head-team-next-game"}).contents))
+
+    next = str(soup.find("p").contents)
+    date = ((next.split('        ')[1]).strip()).split("PM")[0].strip()
+    t = date.split(',')[3].strip()
+    date = date.split(',')[1].strip()
+    m, d = date.split(" ")
+    d = int(d)
+
+    teams = str(soup.find("h4").contents)
+    team1 = (teams.split(">")[1].split("<")[0])
+    team2 = (teams.split(">")[3].split("<")[0])
+    if team1.strip() == "Pittsburgh Steelers":
+        opp = team2.strip()
+    else:
+        opp = team1.strip()
+
+    words = opp.split(" ")
+    if len(words) == 3:
+        opp = words[2]
+    else:
+        opp = words[1]
+
+    return m, d, t, opp
 
 
-foo, rank, record = ranking.split("'")
-rank = rank.strip()
-foo, rec, bar = record.split("<")
-foo, newrec = rec.split(">")
-foo, newrec = newrec.split("(")
-newrec, bar = newrec.split(")")
+if __name__ == "__main__":
+    with timeout(seconds=5):
+        try:
+            rank, record = getRankRecord()
+            month, day, time, opponent = getNextGame()
+            today = dt.today().day
+            print "Steelers (%s, %s):" % (rank, record),
+            if day == today:
+                print "Today, %s" % time
+            else:
+                print "%s %s, %s vs %s" % (month, day, time, opponent)
 
-
-response = newurllib2.urlopen('http://www.sbnation.com/nfl/teams/pittsburgh-steelers').read()
-soup = Soup(response)
-soup = Soup(str(soup.find("div", attrs={"class": "sbn-pte-head-team-next-game"}).contents))
-next = str(soup.find("p").contents)
-foo, date, bar = next.split("        ")
-date = date.strip()
-date, bar = date.split("PM")
-day, date, year, time = date.split(",")
-day = day.strip()
-date = date.strip()
-m, d = date.split(" ")
-d = int(d)
-
-today = datetime.date.today()
-today = today.day
-
-print "Steelers (%s, %s) Next Game:" % (rank, newrec),
-if (d == today):
-    print "Today, %s" % time
-else:
-    print "%s, %s,%s PM" % (day, date, time)
+        except TimeoutError:
+            print ""
