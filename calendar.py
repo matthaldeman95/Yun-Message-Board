5fccfff8f01d5101904deeccf05967821be17d0d
Matthew Haldeman <matthaldeman95@gmail.com>
2016-09-26 16:26:03 -0400

    Functions for timeouts and for getting month number and name

---
+++ calendar.py
@@ -0,0 +1,41 @@
+
+def getMonth(m):
+
+    if m == "January":
+        month = "Jan"
+        month_number= 1
+    elif m == "February":
+        month = "Feb"
+        month_number= 2
+    elif m == "March":
+        month = "Mar"
+        month_number= 3
+    elif m == "April":
+        month = "Apr"
+        month_number= 4
+    elif m == "May":
+        month = "May"
+        month_number= 5
+    elif m == "June":
+        month = "Jun"
+        month_number= 6
+    elif m == "July":
+        month = "Jul"
+        month_number= 7
+    elif m == "August":
+        month = "Aug"
+        month_number= 8
+    elif m == "September":
+        month = "Sep"
+        month_number= 9
+    elif m == "October":
+        month = "Oct"
+        month_number= 10
+    elif m == "November":
+        month = "Nov"
+        month_number= 11
+    elif m == "December":
+        month = "Dec"
+        month_number= 12
+
+    return month, month_number
